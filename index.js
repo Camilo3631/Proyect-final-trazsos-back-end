571872c11024ee602e93b66f6cc8bdb0107d7bca
Camilo Acosta <camiloacosta1193@outlook.com>
2026-06-28 16:42:41 -0500

    routes

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { MongoClient } from 'mongodb';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+
+
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+app.listen(process.env.PORT || 3000);
+
+
+
