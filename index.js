9665e4452252ef015f368e68db071a31a847104c
Camilo Acosta <camiloacosta1193@outlook.com>
2026-06-30 10:08:41 -0500

    ligin y register routes

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 import express from "express";
-import { MongoClient } from 'mongodb';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import { MongoClient } from "mongodb";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-import registerRouter from './routes/register.js';
+import registerRouter from "./routes/register.js";
+import loginRouter from './routes/login.js';
 
 const app = express();
+
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const client = new MongoClient(process.env.MONGODB_URI); 
+// Todas las rutas del router comenzarán con /api
+app.use("/api", registerRouter);
+app.use("/api", loginRouter); 
 
+await connectDB();
 
-const main = async () => {
-    await client.connect();
-    app.locals.db = client.db('medicalcitas');
-    app.use('/api', registerRouter);
-    app.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor corriendo 🚀');
-    });
-};
+app.listen(process.env.PORT || 3000, () => {
+ // console.log(`Servidor escuchando en el puerto ${process.env.PORT || 3000}`);
+});
 
-main();
+async function connectDB() {
+  const client = await MongoClient.connect(process.env.MONGODB_URI);
+  app.locals.db = client.db("medicalcitas");
+  console.log("✅ Conectado a MongoDB");
+}
 
+export default app;
--- routes/login.js
+++ routes/login.js
@@ -2,3 +2,31 @@ import { Router  } from "express";
 import bcrypt from "bcrypt"
 
 const router = Router();
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({mensaje: 'Todos los campos son obligatorios'});
+    }
+
+    const usuario = await req.app.locals.db
+      .collection('users')
+      .findOne({ email });
+ 
+   if (!usuario) {
+      return res.status(401).json({ mensaje: 'Credenciales incorrectas' });
+   }
+
+   const passwordMatch = await bcrypt.compare(password, usuario.password);
+
+   if (!passwordMatch) {
+      return res.status(401).json({ mensaje: 'Credenciales incorrectas'});
+   }
+
+   res.status(200).json({ mensaje: 'Login exitoso', usuario});
+
+})
+
+
+export default router;
